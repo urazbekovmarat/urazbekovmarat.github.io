ebe9fa5808b79249be82ec2c4d9d729818d414c8
urazbekovmarat <urazbekov.marat21@mail.ru>
2025-04-03 16:49:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.querySelector('.button').addEventListener('click',
-function() {
- alert('Вы нажали на кнопку!');
+// Получаем все ссылки навигации
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Получаем все разделы страницы
+const sections = document.querySelectorAll('.section');
+
+// Добавляем обработчик события для каждой ссылки
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Получаем ID секции, на которую ссылается ссылка
+        const targetId = link.getAttribute('href').substring(1);
+
+        // Отображаем нужный раздел, скрывая остальные
+        sections.forEach(section => {
+            if (section.id === targetId) {
+                section.classList.add('active');
+            } else {
+                section.classList.remove('active');
+            }
+        });
+    });
 });
